eab56861d6d18fc56c3f158fd483ee8da4365c8e
Michael Mock <michi@tahina.priv.at>
2019-03-14 18:29:44 +0100

    Removes test-output from fibo_inefficient

--- A1/SpinatMockerl-fibo_inefficient.cpp
+++ A1/SpinatMockerl-fibo_inefficient.cpp
@@ -30,17 +30,6 @@ int main(int argc, char **argv) {
 
   cout << n << ": "<< fibonacci(n, all, nums) << endl;
 
-
-
-
-cout << nums.size() << endl;
-
-//cout << "yeh" << endl;
-
-
-
-
-
   return 0;
 }
 
@@ -48,57 +37,26 @@ cout << nums.size() << endl;
 int fibonacci(int n, bool all, vector<int> &nums, bool outermost, bool append) {
   // Initiiert den Array 'nums' welcher die Fibonacci-Zahlen im Falle von
   // all = true sammelt:
-  //nums[0] = 1; nums[1] = 1;
-
-                                cout << "SIZE: "<< nums.size() << endl;
-
-
-if (outermost) {cout << nums.size() << endl;
-for (int i= 0; i<10; i++){
-    //cout << nums[i] << endl;
-}}
-
-
 
   if (n < 1) {
     cout << "Please input a positive integer." << endl;
   } else if (n == 0) {
       return 0;
   } else if (n < 3) {
-    //nums.push_back(1);
-
-    //cout << "PUSHED BACK" << endl;
     
     cout << "n < 3" << endl;
     
     return 1;
   } else {
-
-    cout << "else" << endl;
-    
     
     int f1 = fibonacci(n -2, all, nums, false, false);
     int f2 = fibonacci(n -1, all, nums, false);
     int f3 = f1 + f2;
 
-                       cout << "f3 == "<< f3 << endl;
-
     if (append) {nums.push_back(f3);}
-               
-                                   cout << "SIZEAFTER: "<< nums.size() << endl;
-
-
-cout << "CLEAR 1"<< endl;
-    
-
 
     if (all && outermost) { // Gibt die Fibonacci folge bis 'n' aus, oder...
       for (int i = 0; i < nums.size() -1; i++) {
-        
-        cout << endl << nums.size() << endl << endl;
-
-
-
         cout << i +1 << ": " << nums[i] << endl;
       }
     }
