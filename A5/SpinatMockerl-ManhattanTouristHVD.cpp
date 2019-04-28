ae0b15ffee8d72540fc26ab79ae90f4edd106e3f
Michael Mock <michi@tahina.priv.at>
2019-04-28 14:43:03 +0200

    Makes A5 nearly functional

--- A5/SpinatMockerl-ManhattanTouristHVD.cpp
+++ A5/SpinatMockerl-ManhattanTouristHVD.cpp
@@ -4,18 +4,16 @@
 #include <vector>
 #include <map>
 #include <unistd.h>
+#include <algorithm>
 using namespace std;
 
 /*To Do:
-UNNEDIG:
-    - neue Funktion zur erstellung von Richtungsmatrizen
-      z.B. Matrix(colSeq.size(), vector<int>(rowSeq.size(), 0)) directionScore;
-        map<int, Matrix> directionscoringFun
-- 
-- needlemanWunschLight KÜBELN
-- neue Backtracker-Funktion
-- Umwandlung in CLUSTAL-Format (eigene Funktion?)
-- Ausgeben
+
+
+Gap-Einbau und Alignment-Länge anpassen
+
+
+
 */
 typedef enum {
   down = 1,
@@ -29,7 +27,8 @@ typedef vector<vector<string>> Fasta;
 map<string, int> argParser(int argc, char** argv);
 Fasta fastaReader();
 Matrix scoreNW(string colSeq, string rowSeq, map<string, int> scoringFun);
-map<string, string> backtrackNW()
+map<string, string> backtrackNW(vector<string> colSeq, vector<string> rowSeq, Matrix scores, map<string, int> scoringFun);
+void printAlignment(map<string, string> alignment);
 
 
 map<string, vector<vector<int>>> pathParser(char delimiter = ' ');
@@ -55,17 +54,19 @@ int main(int argc, char** argv) {
     }
   } */
 
-  for (int i = 0; i < argc; i++) {
+  /*for (int i = 0; i < argc; i++) {
     cout << argv[i] << endl;
-  }
+  }*/
 
-  cout << "works so far" << endl;
+  //cout << "works so far" << endl;
 
   map<string, int> scoringFun = argParser(argc, argv);
 
-  cout << scoringFun["match"] << endl;
-
   vector<vector<string>> seqs = fastaReader();
+  
+  //cout << "SEQUENCE 0:" << endl;
+  //cout << seqs[0][1] << endl;
+  
 
   int seqCount = seqs.size();
 
@@ -73,8 +74,31 @@ int main(int argc, char** argv) {
     for (int col = row +1; col < seqCount; col++) {
       // anpassen an neues Format von seqs:
       Matrix scoringMatrix = scoreNW(seqs[col][1], seqs[row][1], scoringFun);
-      // ~ vector.push_back(Backtracking)
 
+      /*for (int i = 0; i < scoringMatrix.size(); i++) {
+        for (int j = 0; j < scoringMatrix[0].size(); j++) {
+          cout << scoringMatrix[i][j] <<" ";
+        }
+        cout << endl << endl;
+      }*/
+/*
+      cout << "row - col:" << endl << row << "-" << col << endl;
+      cout << "SEQUENCE "<< col << ": "<< endl;
+      cout << seqs[col][0] << endl << seqs[col][1] << endl;
+      cout << "SEQUENCE "<< row << ": "<< endl;
+      cout << seqs[row][0] << endl << seqs[row][1] << endl << endl;
+  */    
+      /*cout << "SCORING MATRIX:" << endl;
+      for (int row = 0; row < scoringMatrix.size(); row++) {
+        for (int col = 0; col < scoringMatrix[0].size(); col++) {
+          cout << scoringMatrix[row][col] << " ";
+        }
+        cout << endl;
+      }*/
+
+      // ~ vector.push_back(Backtracking)
+      map <string, string> alignment = backtrackNW(seqs[col], seqs[row], scoringMatrix, scoringFun);
+      printAlignment(alignment);
     }
   }
 
@@ -87,7 +111,7 @@ int main(int argc, char** argv) {
 map<string, int> argParser(int argc, char** argv) {
   map<string, int> scoringFun = {{"match", 1}, {"mismatch", -1}, {"gap", -2}};
 
-  cout << "inside function" << endl;
+  //cout << "inside function" << endl;
 
   for (int i = 1; i < argc; i++) {
     string arg = (string)argv[i];
@@ -119,13 +143,15 @@ Fasta fastaReader() {
   string seq;
   Fasta seqs;
   string header;
+  bool firstLine = true;
 
   while (getline(cin, line)) {
-    cout << "isatty: "<< isatty(STDIN_FILENO) << endl; //STDIN_FILENO = 0
-
     if (line[0] == '>') {
       vector<string> compoundSeq = {header, seq};
-      seqs.push_back(compoundSeq);
+      
+      if (! firstLine) {
+        seqs.push_back(compoundSeq);
+      }
       
       // Set to next Sequence:
       header = line;
@@ -135,10 +161,16 @@ Fasta fastaReader() {
     } else {
       seq += line;
     }
+
+    //cout << "line read" << endl;
+
+    firstLine = false;
   }
   
   vector<string> compoundSeq = {header, seq};
   seqs.push_back(compoundSeq);
+
+  //cout << "all sequences read" << endl;
 
   return seqs;
 }
@@ -148,10 +180,19 @@ Matrix scoreNW(string colSeq, string rowSeq, map<string, int> scoringFun) {
 
   for (int row = 0; row < rowSeq.size() +1; row++) {
     for (int col = 0; col < colSeq.size() +1; col++) {
-      if ((row & col) == 0) {
+
+      //cout << "while scoring: row - col = " << row << "-" << col << endl;
+
+      if (row == 0 && col == 0) {
+        //cout << "ROW AND COL == 0" << endl;
+
         continue;
       } else if (row == 0) {
         scores[row][col] = scores[row][col -1] + scoringFun["gap"];
+
+        /*cout << "GAP-Cost = " << scoringFun["gap"] << endl;
+        cout << "Score to the left = "<< scores[row][col -1] << endl;
+        cout << "calculated score = "<< scores[row][col] << endl;*/
       } else if (col == 0) {
         scores[row][col] = scores[row -1] [col] + scoringFun["gap"];
       } else {
@@ -170,8 +211,212 @@ Matrix scoreNW(string colSeq, string rowSeq, map<string, int> scoringFun) {
     }
   }
 
+  //cout << "NW-score calculated" << endl;
+
   return scores;
 }
+
+map<string, string> backtrackNW(vector<string> colSeq, vector<string> rowSeq, Matrix scores, map<string, int> scoringFun) {
+  vector<char> rowAln;
+  vector<char> colAln;
+  vector<char> seqDecoration;
+
+  int row = scores.size() -1;
+  int col = scores[0].size() -1;
+
+        /*cout << "ROW:" << endl << rowSeq[1][17] << endl;
+        cout << "COL:" << endl << colSeq[1][16] << endl;*/
+
+  while (row > 0) {
+
+    //cout << "while (row > 0) {" << endl;
+
+    if (col < 1) {
+      break;
+    }
+
+    while (col > 0) {
+
+      /*cout << "while (col > 0) {" << endl;
+      cout << row << "-" << col << endl;
+
+      cout << "CURRENT SCORE = "<< scores[row][col] << endl;
+      cout << "DIAGONALLY PREVIOUS SCORE = "<< scores[row][col] << endl;
+
+      if (row == 16 && col == 15) {
+        cout << "ROW:" << endl << rowSeq[1][15] << endl;
+        cout << "COL:" << endl << colSeq[1][14] << endl;
+      }*/
+
+      if (row < 1) {
+        break;
+      }
+
+      if (scores[row][col] == scores[row -1][col -1] + scoringFun["match"]
+          && rowSeq[1][row -1] == colSeq[1][col -1]) {
+        rowAln.insert(rowAln.begin(), rowSeq[1][row -1]);
+        colAln.insert(colAln.begin(), colSeq[1][col -1]);
+        seqDecoration.insert(seqDecoration.begin(), '*');
+
+        row--;
+        col--;
+      } else if (scores[row][col] == scores[row -1][col -1] + scoringFun["mismatch"]) {
+        rowAln.insert(rowAln.begin(), rowSeq[1][row -1]);
+        colAln.insert(colAln.begin(), colSeq[1][col -1]);
+        seqDecoration.insert(seqDecoration.begin(), ' ');
+
+        row--;
+        col--;
+      } else if (scores[row][col] == scores[row][col -1] + scoringFun["gap"]) {
+        rowAln.insert(rowAln.begin(), '-');
+        colAln.insert(colAln.begin(), colSeq[1][col -1]);
+        seqDecoration.insert(seqDecoration.begin(), ' ');
+
+        col--;
+      } else if (scores[row][col] == scores[row -1][col] + scoringFun["gap"]) {
+        rowAln.insert(rowAln.begin(), rowSeq[1][row -1]);
+        colAln.insert(colAln.begin(), '-');
+        seqDecoration.insert(seqDecoration.begin(), ' ');
+
+        row--;
+      }
+    }
+  }
+  // fill in remaining gaps, if row or col == 0
+  
+  //cout << "filling in remaining gaps..." << endl;
+
+  while (row > 0) {
+    rowAln.insert(rowAln.begin(), rowSeq[1][row -1]);
+    seqDecoration.insert(seqDecoration.begin(), ' ');
+    row--;
+  }
+
+  while (col > 0) {
+    colAln.insert(colAln.begin(), colSeq[1][col -1]);
+    seqDecoration.insert(seqDecoration.begin(), ' ');
+    col--;
+  }
+
+  //map<string, string> alignment = {{"seq1", colAln}, {"header1", colSeq[0]}, {"seq2", rowAln}, {"header2", rowSeq[0]}, {"seqDecoration", seqDecoration}};
+
+  /*colAln(colAln.begin(), colAln.end());
+  rowAln(rowAln.begin(), rowAln.end());*/
+
+  //cout << "building alignment..." << endl;
+
+  map<string, string> alignment;
+  //alignment["seq1"] = colAln;
+  alignment["header1"] = colSeq[0];
+  //alignment["seq2"] = rowAln;
+  alignment["header2"] = rowSeq[0];
+  //alignment["seqDecoration"] = seqDecoration;
+
+  for (int i = 0; i < rowAln.size(); i++) {
+    alignment["seq1"] += colAln[i];
+    alignment["seq2"] += rowAln[i];
+    alignment["seqDecoration"] += seqDecoration[i];
+  }
+
+  //cout << "ALIGNMENT 1: "<< alignment["seq2"] << endl;
+  
+  
+  return alignment;
+}
+
+void printAlignment(map<string, string> alignment) {
+  //cout << "printing alignment..." << endl;
+  
+  int seqPos = 0;
+  string seqID = "1";
+
+  while (seqPos < alignment["seq1"].size()) {
+    for (int headerPos = 0; headerPos < 20; headerPos++) {
+      if (alignment["header" + seqID].size() > headerPos) {
+        cout << alignment["header" + seqID][headerPos];
+      } else {
+        cout << " ";
+      }
+    }
+
+    for (int seqLinePos = 0; seqLinePos < 60; seqLinePos++) {
+      if (seqLinePos > alignment["seq" + seqID].size()) {
+        break;
+      }
+
+      //cout << alignment["seq" + seqID][seqLinePos];
+
+      /*if (seqID == "2") {
+        cout << endl;
+        */if (seqID == "seqDecoration") {
+        for (int pos = 0; pos < 20; pos++) {
+          cout << " ";
+        //}
+        }}
+        cout << alignment["seq" + seqID][seqPos];
+      //}
+      seqPos++;
+    }
+    
+    //cout << seqID << endl;
+
+    //cout << "got it ?" << endl;
+
+    if (seqID != "Decoration") {
+      if (seqPos < 60) {
+        seqPos -= alignment["seq1"].size();
+      } else {
+        seqPos -= 60;
+      }
+
+      if (seqID == "1") { //cout << endl << "switched to 2" << endl;
+        seqID = "2"; 
+        //cout << "seqID = "<< seqID << endl;
+      } else if (seqID == "2") { //cout << endl << "switched to Decoration" << endl;
+        seqID = "Decoration";
+      }
+
+    } else /*if (seqID == "2")*/ { //cout << endl << "switched to 1" << endl;
+      /*seqID = "Decoration";
+    } else {
+      */seqID = "1";
+      
+      /*cout << endl;
+
+      for (int pos = 0; pos < 20; pos++) {
+        cout << " ";
+      }
+
+      cout << alignment["seqDecoration"] << endl;*/
+    }
+    cout << endl;
+
+
+    //cout << "seqID: " << seqID << endl;
+    
+    
+    //cout << seqID << endl;
+    //cout << seqPos << endl;
+  } // decoration ausgeben, endls
+
+
+/*
+  for (int pos = 0; pos < 80; pos++) {
+    for (int headerPos = 0; headerPos < 20) {
+      if (alignment["header1"].size() > headerPos) {
+        cout << alignment["header1"][headerPos];
+        pos++;
+      } else {
+        cout << " ";
+        pos++;
+      }
+    } //0-19;20-79
+    
+    while (pos < 80) {
+      c
+    }
+  }*/
+}//for (int baseNo = 0; baseNo < alignment["seq1"].size(); baseNo++) {
 
 /*map<int, Matrix> directionalscoringFun(string colSeq, string rowSeq, map<string, int> scoringFun) {
   map<int, Matrix> directionalMatrix = {
